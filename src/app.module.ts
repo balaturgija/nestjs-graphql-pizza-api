231998f66248b60f77000f43d617a4ba82389044
balaturgija <matej.balat@gmail.com>
2023-08-31 16:02:35 +0200

    Create app resolver and register graphql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 
 import { DatabaseModule } from '~modules/database';
 
 import { AppController } from './app.controller';
+import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
 
 @Module({
@@ -12,9 +15,14 @@ import { AppService } from './app.service';
       envFilePath: `.env`,
       isGlobal: true,
     }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: true,
+    }),
     DatabaseModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
